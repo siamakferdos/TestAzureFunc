de1bd711b725ec5cdefc1a37b713dba85143ce88
Siamak Ferdos <siamak@caelor.com>
2023-02-11 09:59:06 +0100

    Add project files.

---
+++ DeleteUsers.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace SiaFunctionApp
+{
+    public class DeleteUsers
+    {
+        private IConfiguration _configuration;
+        public DeleteUsers(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [FunctionName("DeleteUsers")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            
+            SqlConnection sqlConnection = new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
+            SqlCommand sqlCommand = new SqlCommand("DELETE From Users", sqlConnection);
+            sqlConnection.Open();   
+            await sqlCommand.ExecuteNonQueryAsync();
+            sqlConnection.Close();
+
+            return new OkObjectResult("Deleted");
+        }
+    }
+}
